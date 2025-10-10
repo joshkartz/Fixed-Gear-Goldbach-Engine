4d9ebd6d8e65a8625c5511024d54823dd55fd706
YoshiRulz <OSSYoshiRulz+git@gmail.com>
2025-10-10 18:09:01 +1000

    Bump to .NET 10

--- shell.nix
+++ shell.nix
@@ -1,9 +1,9 @@
 { system ? builtins.currentSystem
 , pkgs ? import (builtins.fetchTarball {
-	url = "https://github.com/NixOS/nixpkgs/archive/a8f575995434695a10b574d35ca51b0f26ae9049.tar.gz"; # commit immediately before .NET 5 was removed
-	sha256 = "0glawbqvvvbiz1gn7i1skhaqw96ilgdds6gzq7mj29kfk4vkzng1";
+	url = "https://github.com/NixOS/nixpkgs/archive/43b6e7fada3593e196fd76cea8888acc0bdd955e.tar.gz"; # first commit with (working) .NET 10 RC1
+	sha256 = "1w086fx55gj60pp2zf4kmnyqgkxgdrf5fj6winbazz24mdm41zkf";
 }) { inherit system; }
 }:
 pkgs.mkShell {
-	packages = [ pkgs.dotnet-sdk_5 ];
+	packages = [ pkgs.dotnet-sdk_10 ];
 }
